9fb247871ffab234f49cc449fcdce77a3029a502
JonahRosenbloom <jonahrosenbloom@gmail.com>
2026-09-20 08:02:59 -0400

    Create Contact.jsx

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,11 @@
+export default function Contact() {
+  return (
+    <div className="section">
+      <h1>Contact Me</h1>
+
+      <p>Email: <strong>your.email@example.com</strong></p>
+      <p>LinkedIn: <a href="https://linkedin.com/in/YOUR_PROFILE">Your Profile</a></p>
+      <p>GitHub: <a href="https://github.com/YOUR_USERNAME">github.com/YOUR_USERNAME</a></p>
+    </div>
+  );
+}
