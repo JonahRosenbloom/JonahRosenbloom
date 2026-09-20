9c6e9b8bd13d14f8eae110dc735dddee50a97996
JonahRosenbloom <jonahrosenbloom@gmail.com>
2026-09-20 07:55:41 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import GitHub from "./pages/GitHub";
+import PCB from "./pages/PCB";
+import Contact from "./pages/Contact";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/github" element={<GitHub />} />
+        <Route path="/pcb" element={<PCB />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
+  );
+}
